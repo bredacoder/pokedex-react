ffa899621212a3ac9cf861fae009f49c00711160
Daniel Felipe Developer <danielfelipedeveloper@gmail.com>
2020-10-06 18:03:41 -0400

    Refactor: Pokemon Images

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -6,8 +6,9 @@ import './styles.css'
 const PokemonCard = ({ pokemon, url }) => {
   const [types, setTypes] = useState([]);
 
-  const pokemonIndex = url.split("/")[url.split('/').length - 2];
+  const pokemonIndex = Number(url.split("/")[url.split('/').length - 2]);
   const imageUrl = `https://pokeres.bastionbot.org/images/pokemon/${pokemonIndex}.png`
+  const imageUrl2 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`
 
   useEffect(() => {
     async function loadPokemon() {
@@ -23,12 +24,24 @@ const PokemonCard = ({ pokemon, url }) => {
     loadPokemon();
   }, [pokemonIndex]);
 
+  function handleImage(id) {
+    if(id === 412||id === 413||id === 421||id === 487||id === 492||id === 585||
+      id === 586 ||id === 641||id === 642||id === 647||id === 648||id === 718||
+      id === 720 ||id === 741||id === 745||id === 746||id === 774||id === 778||
+      id === 849 ||id === 875||id === 877||id === 888||id === 889||id === 891||
+      id === 892 ||id === 893) {
+        return imageUrl2
+      } else {
+        return imageUrl
+      }
+  }
+
   return (
     <div className="pokemon">
 
       <figure className="pokemon-figure">
         <img
-          src={imageUrl}
+          src={handleImage(pokemonIndex)}
           alt=""
         />
       </figure>
