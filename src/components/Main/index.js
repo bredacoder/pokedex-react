d102c676dc2955afc826c6ed00bed617c40517b8
Daniel Felipe Developer <danielfelipedeveloper@gmail.com>
2020-10-08 11:02:45 -0400

    atualizando...

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,25 +7,18 @@ import PokemonCard from "../PokemonCard";
 const Main = () => {
   const [pokemons, setPokemons] = useState([]);
   const [filteredPokemons, setFilteredPokemons] = useState([]);
+  const [sortPokemons, setSortPokemons] = useState([]);
   const [name, setName] = useState();
-  const [types, setTypes] = useState();
   const inputRef = useRef(null);
 
-  console.log(types)
-
-  const options = [
-    { value: 'normal' }, { value: 'fighting' }, { value: 'flying' }, { value: 'poison' },
-    { value: 'ground' }, { value: 'rock' }, { value: 'bug' }, { value: 'ghost' },
-    { value: 'steel' }, { value: 'fire' }, { value: 'water' }, { value: 'grass' },
-    { value: 'electric' }, { value: 'psychic' }, { value: 'ice' }, { value: 'dragon' },
-    { value: 'dark' }, { value: 'fairy' }
-  ]
+  // console.log(sortPokemons)
 
   useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon?limit=150")
       .then((response) => {
         setPokemons(response.data["results"]);
+        setFilteredPokemons(response.data["results"])
       });
   }, []);
 
@@ -34,11 +27,22 @@ const Main = () => {
       setFilteredPokemons(
         pokemons.filter((pokemon) => pokemon.name.includes(name))
       );
-    } else {
-      setFilteredPokemons(pokemons);
+    }
+    else {
+      setFilteredPokemons(pokemons)
     }
   }, [pokemons, name]);
-  
+
+  useEffect(() => {
+    if (sortPokemons === "A-Z") {
+      const filtered = pokemons.sort(function (a, b) {
+        return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+      })
+      setFilteredPokemons(filtered)
+    }
+  }, [pokemons, sortPokemons])
+
+
 
   return (
     <main>
@@ -56,29 +60,8 @@ const Main = () => {
         </div>
 
         <div className="form-control">
-          <label htmlFor="filter-type">Type:</label>
-          <select id="filter-type"
-            onChange={e => {setTypes(e.target.value)}}
-          >
-            <option value="undefined">All</option>
-            {options.map(option => (
-              <option key={option.value} value={option.value}>{option.value}</option>
-            ))}
-          </select>
-        </div>
-
-{/* 
-        onChange={e => { setTypes(e.target.value) }}
-            options={[
-              { value: 'normal' }, { value: 'fighting' }, { value: 'flying' }, { value: 'poison' },
-              { value: 'ground' }, { value: 'rock' }, { value: 'bug' }, { value: 'ghost' },
-              { value: 'steel' }, { value: 'fire' }, { value: 'water' }, { value: 'grass' },
-              { value: 'electric' }, { value: 'psychic' }, { value: 'ice' }, { value: 'dragon' },
-              { value: 'dark' }, { value: 'fairy' }
-            ]} */}
-        <div className="form-control">
           <label htmlFor="sort">Sort:</label>
-          <select id="sort-type">
+          <select id="sort-type" onChange={e => { setSortPokemons(e.target.value) }}>
             <option>Lowest Number (First)</option>
             <option>Highest Number (First)</option>
             <option>A-Z</option>
